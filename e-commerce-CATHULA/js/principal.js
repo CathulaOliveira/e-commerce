4695bed5962d5da27a0f865ca8e256e66d330d02
Cathula Oliveira <cathulapameladeoliveira@gmail.com>
2019-12-04 00:00:30 -0300

    implementações nas classes javascript

--- e-commerce-CATHULA/js/principal.js
+++ e-commerce-CATHULA/js/principal.js
@@ -13,4 +13,28 @@ $(function () {
         e.preventDefault();
         window.location.href = "carrinho.html";
     })
+    
+    $('#btAdicionar').click(function (e) {
+        e.preventDefault();
+        aumentarQtdeProdutos();
+        alertaItemAdicionado();
+        
+    })
 })
+
+function alertaItemAdicionado() {
+    alert("Item adicionado no carrinho");
+}
+
+function aumentarQtdeProdutos() {
+    let qtde = new Number($('#qtdeProdutos').val());
+    $('#qtdeProdutos').val(qtde + 1);
+}
+
+function setArrayStorage(arr) {
+    localStorage.setItem("listaProduto", JSON.stringify(arr));
+}
+
+function getArrayStorage() {
+    return JSON.parse(localStorage.getItem("listaProduto"));
+}
